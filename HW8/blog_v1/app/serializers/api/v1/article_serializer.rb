2c3c2ffcb8db398361688dcb1b5d64316575e50a
fillson22 <fillson22051983@gmail.com>
2022-12-12 19:23:55 +0200

    Add Serializers

--- HW8/blog_v1/app/serializers/api/v1/article_serializer.rb
+++ HW8/blog_v1/app/serializers/api/v1/article_serializer.rb
@@ -1,5 +1,10 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :created_at
+  attributes :id, :title, :body, :created_at, :author
 
   has_many :comments
+
+  def author
+    author = Author.find_by(id: object.author_id)
+    "#{author.name}"
+  end
 end
--- HW8/blog_v1/app/serializers/api/v1/comment_serializer.rb
+++ HW8/blog_v1/app/serializers/api/v1/comment_serializer.rb
@@ -1,5 +1,10 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body, :status, :created_at
+  attributes :id, :body, :status, :created_at, :author
   
   belongs_to :article
+
+  def author
+    author = Author.find_by(id: object.author_id)
+    "#{author.name}"
+  end
 end
