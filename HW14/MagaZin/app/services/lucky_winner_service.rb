b39a37db214131dd5b3c121247849bc217724df9
fillson22 <fillson22051983@gmail.com>
2023-02-02 22:16:02 +0200

    Fixed services

--- HW14/MagaZin/app/services/lucky_winner_service.rb
+++ HW14/MagaZin/app/services/lucky_winner_service.rb
@@ -1,16 +1,15 @@
 class LuckyWinnerService
-  
 
-  
-    # puts "Hi #{user.sample.email}"
-    users = User.all
-    orders = Order.all
-    user = users.sample
-
-    users.each do |user|
+  def call
+    orders_month = Order.where("created_at >= ?", 1.week.ago.utc)
+    orders_paid = orders_month.where(:status => 'paid')
+    orders_paid.each do |order|
+      u_user << order.user
+    end
+    lucky_users = u_user.sample(2)
+    lucky_users.each do |user|
       SaleMailer.lucky_user(user).deliver_later
     end
-    
-
+  end
 
 end
