1c1d7bbb9fdc2e1116146a2c4a6b9e953d0564d9
fillson22 <fillson22051983@gmail.com>
2023-02-06 14:31:30 +0200

    HW15 init

---
+++ HW15/MagaZin/app/models/product.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  price       :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_products_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
+class Product < ApplicationRecord
+  belongs_to :category
+  has_many :line_items, dependent: :destroy
+  has_one_attached :image, dependent: :destroy do |attachable|
+    attachable.variant :small, resize_to_limit: [50, 50]
+    attachable.variant :norm, resize_to_limit: [100, 100]
+  end
+end
